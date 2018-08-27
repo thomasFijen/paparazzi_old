18377f760cf9fe92e484ea1905b83d445756d6c7
thomasFijen <thomasfijen@yahoo.com>
2018-08-27 13:08:42 +0200

    Recreated the UWB localisation

--- sw/airborne/modules/decawave/uwb_localisation_and_comms.c
+++ sw/airborne/modules/decawave/uwb_localisation_and_comms.c
@@ -18,15 +18,12 @@
  * <http://www.gnu.org/licenses/>.
  */
 /**
- * @file "modules/decawave/uwb_dw1000_delft.c"
+ * @file "modules/decawave/uwb_localisation_and_comms.c"
  * @author Thomas Fijen
- * This is a driver to get ranging data from the Decawave DW1000 connected to Arduino. The format of the data recieved over the serial link is: [(byte) START_MARKER, (byte) anchorID, (float) range]. This module is based off of the module 'dw1000_arduino' created by Gautier Hattenberger <gautier.hattenberger@enac.fr>
- */
- /*
- *	NOTE: To do this I had to edit the GPS.h file (line 166). I changed the value of GPS_TIMEOUT from 2 to 5
+ * This function contains the code necessary to localise the UAV, using UWB, as well as the inter-UAV communication
  */
 
-#include "modules/decawave/uwb_dw1000_delft.h"
+#include "modules/decawave/uwb_localisation_and_comms.h"
 //#include "modules/decawave/multilateration_ls.h"
 #include "modules/decawave/multilateration_nlls.h"
 //#include "modules/decawave/trilateration.h"
@@ -75,13 +72,9 @@
 #define DW1000_TIMEOUT 500
 #endif
 
-/** Parsing states */
-//#define DW_WAIT_STX 0
-//#define DW_GET_DATA 1
-//#define DW_GET_CK 2
+
 
 #define DW_NB_DATA 5
-
 #define START_MARKER 254
 
 static bool _inProgress = false;
@@ -112,17 +105,17 @@ struct DW1000 {
 static struct DW1000 dw1000;
 
 void getRanges(float ranges[4]){
-	ranges[0] = dw1000.anchors[0].distance;
-	ranges[1] = dw1000.anchors[1].distance;
-	ranges[2] = dw1000.anchors[2].distance;
-	if(DW1000_NB_ANCHORS == 4)
-	{
-		ranges[3] = dw1000.anchors[3].distance;
-	}
-	else
-	{
-		ranges[3] = 0;
-	}
+  ranges[0] = dw1000.anchors[0].distance;
+  ranges[1] = dw1000.anchors[1].distance;
+  ranges[2] = dw1000.anchors[2].distance;
+  if(DW1000_NB_ANCHORS == 4)
+  {
+    ranges[3] = dw1000.anchors[3].distance;
+  }
+  else
+  {
+    ranges[3] = 0;
+  }
 }
 
 
@@ -145,25 +138,24 @@ static inline uint16_t uint16_from_buf(uint8_t* b) {
 
 /** Utility function to fill anchor from buffer */
 static void fill_anchor_Cust(struct DW1000 *dw) {
-   uint16_t id = uint16_from_buf(dw->buf);
+  uint16_t id = uint16_from_buf(dw->buf);
     
   for (int i = 0; i < DW1000_NB_ANCHORS; i++) {
     if (dw->anchors[i].id == id) {
     float norm = sqrtf((dw->anchors[i].distance - float_from_buf(dw->buf+1))*(dw->anchors[i].distance - float_from_buf(dw->buf+1)));
-    	if (norm < 2 || dw->anchors[i].distance == 0) { //This is a check to reject outlier distance measurements
-		
-		  dw->anchors[i].distance = float_from_buf(dw->buf+1);
-		  dw->anchors[i].time = get_sys_time_float();
-		  dw->updated = true;
-	//	  printf("ID: %d, - Range: %f \n",id,float_from_buf(dw->buf+1));
-   		}
-    	else
-    	{
-    		dw->anchors[i].distance = dw->anchors[i].distance;
-		    dw->anchors[i].time = get_sys_time_float();
-		    dw->updated = true;
-		}
-    	
+      if (norm < 2 || dw->anchors[i].distance == 0) { //This is a check to reject outlier distance measurements
+    
+      dw->anchors[i].distance = float_from_buf(dw->buf+1);
+      dw->anchors[i].time = get_sys_time_float();
+      dw->updated = true;
+  //    printf("ID: %d, - Range: %f \n",id,float_from_buf(dw->buf+1));
+      }
+      else
+      {
+        dw->anchors[i].distance = dw->anchors[i].distance;
+        dw->anchors[i].time = get_sys_time_float();
+        dw->updated = true;
+    }
       break;
     }
   }
@@ -171,31 +163,28 @@ static void fill_anchor_Cust(struct DW1000 *dw) {
 
 /** Data parsing function */
 static void dw1000_arduino_parse(struct DW1000 *dw)
-{		
-		
-	while (uart_char_available(&UWB_DW1000_DEV)){
-		_varByte = uart_getch(&UWB_DW1000_DEV);
-		
-		if (_inProgress){
-			dw->buf[dw->idx] = _varByte;
-			dw->idx++;
-		}
-		if (dw->idx == DW_NB_DATA){
-			_inProgress = false;
-			fill_anchor_Cust(dw);
-		}
-		
-		if (_varByte == START_MARKER){
-			dw->idx = 0;
-			_inProgress = true;
-		}
-	}
-		
+{   
+  while (uart_char_available(&UWB_DW1000_DEV)){
+    _varByte = uart_getch(&UWB_DW1000_DEV);
+    
+    if (_inProgress){
+      dw->buf[dw->idx] = _varByte;
+      dw->idx++;
+    }
+    if (dw->idx == DW_NB_DATA){
+      _inProgress = false;
+      fill_anchor_Cust(dw);
+    }
+    
+    if (_varByte == START_MARKER){
+      dw->idx = 0;
+      _inProgress = true;
+    }
+  }  
 }
 
 static void send_gps_dw1000_small(struct DW1000 *dw)
 {
-
   // rotate and convert to cm integer
   float x = dw->pos.x * cosf(dw->initial_heading) - dw->pos.y * sinf(dw->initial_heading);
   float y = dw->pos.x * sinf(dw->initial_heading) + dw->pos.y * cosf(dw->initial_heading);
@@ -205,24 +194,24 @@ static void send_gps_dw1000_small(struct DW1000 *dw)
   float error = sqrtf((aveX[count-1]-x)*(aveX[count-1]-x)+(aveY[count-1]-y)*(aveY[count-1]-y));
   if (error > 10)
   {
-  	if(count == 0)
-  	{
-		aveX[count] = aveX[4];
-		aveY[count] = aveY[4];
-		aveZ[count] = aveZ[4];
-	}
-	else
-	{
-		aveX[count] = aveX[count-1];
-		aveY[count] = aveY[count-1];
-		aveZ[count] = aveZ[count-1];
-	}
+    if(count == 0)
+    {
+    aveX[count] = aveX[4];
+    aveY[count] = aveY[4];
+    aveZ[count] = aveZ[4];
   }
   else
   {
-	  aveX[count] = x;
-	  aveY[count] = y;
-	  aveZ[count] = dw->pos.z;
+    aveX[count] = aveX[count-1];
+    aveY[count] = aveY[count-1];
+    aveZ[count] = aveZ[count-1];
+  }
+  }
+  else
+  {
+    aveX[count] = x;
+    aveY[count] = y;
+    aveZ[count] = dw->pos.z;
   }//--End outlier rejection
   
   // -- Moving average Filter
@@ -232,9 +221,9 @@ static void send_gps_dw1000_small(struct DW1000 *dw)
   
   for(int i=0;i<5;i++)
   {
-	x=x+aveX[i];
- 	y=y+aveY[i];
-	z=z+aveZ[i];
+  x=x+aveX[i];
+  y=y+aveY[i];
+  z=z+aveZ[i];
   }
   x=x/5;
   y=y/5;
@@ -242,11 +231,11 @@ static void send_gps_dw1000_small(struct DW1000 *dw)
   
   if(count == 4)
   {
-  	count = 0;
+    count = 0;
   }
   else
   {
-  	count++;
+    count++;
   }
   //-- End of the filters and outlier rejection --
   
@@ -282,7 +271,7 @@ static void send_gps_dw1000_small(struct DW1000 *dw)
   lla_of_ecef_i(&(dw->gps_dw1000.lla_pos), &(dw->gps_dw1000.ecef_pos));
   SetBit(dw->gps_dw1000.valid_fields, GPS_VALID_POS_LLA_BIT);
 
-  dw->gps_dw1000.hmsl = dw->ltp_def.hmsl + enu_pos.z * 10;		
+  dw->gps_dw1000.hmsl = dw->ltp_def.hmsl + enu_pos.z * 10;    
   SetBit(dw->gps_dw1000.valid_fields, GPS_VALID_HMSL_BIT);
 
   dw->gps_dw1000.num_sv = 7;
@@ -298,36 +287,32 @@ static void send_gps_dw1000_small(struct DW1000 *dw)
   // publish new GPS data
   uint32_t now_ts = get_sys_time_usec();
   
-    //printf("%f,%f,%f,%f,%f,%f \n",dw->anchors[0].distance,dw->anchors[1].distance,dw->anchors[2].distance,dw->anchors[3].distance,x,y);
-  //printf("%f,%f,%f,%f,%f,%f,%f,%f,%f \n",dw->anchors[0].distance,dw->anchors[1].distance,dw->anchors[2].distance,dw->anchors[3].distance,x,y,(*pos2).x,(*pos2).y,(*pos2).z); 
-  
-  
-	// -- Sending the position to the Auto pilot
+  // -- Sending the position to the Auto pilot
   update_uwb(now_ts, &(dw->gps_dw1000));
 }
 
 //----------------------------------------------------
 /* Added a Kalman Filter to the position. Not sure if this will affect the other filters in the INS...
-*	Inputs: Previously Calculated X, inputs u, UWB pos x, UWB pos y
-		u = [vel X; vel Y]; X = [accel x, vel x, accel y, vel y, x, y]
+* Inputs: Previously Calculated X, inputs u, UWB pos x, UWB pos y
+    u = [vel X; vel Y]; X = [accel x, vel x, accel y, vel y, x, y]
 */
 void kalman_filter(float out[6], float X_old[6], float u[2], float x, float y)
 {
-	//--Discretised model
-	float phi[36] = {0.8942,-2.4404,0,0,0,0,0.0018,0.9976,0,0,0,0,0,0,0.8942,-2.4404,0,0,0,0,0.0018,0.9976,0,0,0,0.002,0,0,1,0,0,0,0,0.002,0,1};
-	float gamma[12] = {2.4404,0,0.0024,0,0,2.4404,0,0.0024,0,0,0,0};
-	float Cd[12] = {0,0,0,0,1,0,0,0,0,0,0,1};
-	
-	float K[12] = {-0.008*1.0e-4,0,0,0,0.001*1.0e-4,-0.0049*1.0e-4,0,0.0001*1.0e-4,0.7365*1.0e-4,-0.009*1.0e-4,-0.009*1.0e-4,0.9060*1.0e-4};
-	float X_hat[6] = {0,0,0,0,0,0};
-	
-	//--Temporary storage variables
-	float temp_6x1[6];
-	float temp2_6x1[6];
-	float temp_2x1[2];
-	float temp2_2x1[2] = {x,y};
-	
-	//Determine X_hat from the model 
+  //--Discretised model, This is hard coded from the MATLAB file.
+  float phi[36] = {0.8942,-2.4404,0,0,0,0,0.0018,0.9976,0,0,0,0,0,0,0.8942,-2.4404,0,0,0,0,0.0018,0.9976,0,0,0,0.002,0,0,1,0,0,0,0,0.002,0,1};
+  float gamma[12] = {2.4404,0,0.0024,0,0,2.4404,0,0.0024,0,0,0,0};
+  float Cd[12] = {0,0,0,0,1,0,0,0,0,0,0,1};
+  
+  float K[12] = {-0.008*1.0e-4,0,0,0,0.001*1.0e-4,-0.0049*1.0e-4,0,0.0001*1.0e-4,0.7365*1.0e-4,-0.009*1.0e-4,-0.009*1.0e-4,0.9060*1.0e-4};
+  float X_hat[6] = {0,0,0,0,0,0};
+  
+  //--Temporary storage variables
+  float temp_6x1[6];
+  float temp2_6x1[6];
+  float temp_2x1[2];
+  float temp2_2x1[2] = {x,y};
+  
+  //Determine X_hat from the model 
     mat_mult_6x6_6x1(temp_6x1, phi, X_old);
     mat_mult_6x2_2x1(temp2_6x1, gamma, u);
     mat_add_6x1(X_hat,temp_6x1,temp2_6x1);
@@ -337,47 +322,45 @@ void kalman_filter(float out[6], float X_old[6], float u[2], float x, float y)
     mat_subtract_2x1(temp_2x1,temp2_2x1,temp_2x1);
     mat_mult_6x2_2x1(temp_6x1,K,temp_2x1);
     mat_add_6x1(out,X_hat,temp_6x1);
-    
-    
 }
 
 //--Matrix functions for the Kalman filter
 void mat_mult_6x6_6x1(float out[6], float in1[36], float in2[6])
 {
-	out[0] = in1[0]*in2[0] + in1[1]*in2[1] + in1[2]*in2[2] + in1[3]*in2[3] + in1[4]*in2[4] + in1[5]*in2[5];
-	out[1] = in1[6]*in2[0] + in1[7]*in2[1] + in1[8]*in2[2] + in1[9]*in2[3] + in1[10]*in2[4] + in1[11]*in2[5];
-	out[2] = in1[12]*in2[0] +in1[13]*in2[1] + in1[14]*in2[2] + in1[15]*in2[3] + in1[16]*in2[4] + in1[17]*in2[5];
-	out[3] = in1[18]*in2[0] +in1[19]*in2[1] + in1[20]*in2[2] + in1[21]*in2[3] + in1[22]*in2[4] + in1[23]*in2[5];
-	out[4] = in1[24]*in2[0] +in1[25]*in2[1] + in1[26]*in2[2] + in1[27]*in2[3] + in1[28]*in2[4] + in1[29]*in2[5];
-	out[5] = in1[30]*in2[0] +in1[31]*in2[1] + in1[32]*in2[2] + in1[33]*in2[3] + in1[34]*in2[4] + in1[35]*in2[5];
+  out[0] = in1[0]*in2[0] + in1[1]*in2[1] + in1[2]*in2[2] + in1[3]*in2[3] + in1[4]*in2[4] + in1[5]*in2[5];
+  out[1] = in1[6]*in2[0] + in1[7]*in2[1] + in1[8]*in2[2] + in1[9]*in2[3] + in1[10]*in2[4] + in1[11]*in2[5];
+  out[2] = in1[12]*in2[0] +in1[13]*in2[1] + in1[14]*in2[2] + in1[15]*in2[3] + in1[16]*in2[4] + in1[17]*in2[5];
+  out[3] = in1[18]*in2[0] +in1[19]*in2[1] + in1[20]*in2[2] + in1[21]*in2[3] + in1[22]*in2[4] + in1[23]*in2[5];
+  out[4] = in1[24]*in2[0] +in1[25]*in2[1] + in1[26]*in2[2] + in1[27]*in2[3] + in1[28]*in2[4] + in1[29]*in2[5];
+  out[5] = in1[30]*in2[0] +in1[31]*in2[1] + in1[32]*in2[2] + in1[33]*in2[3] + in1[34]*in2[4] + in1[35]*in2[5];
 }
 void mat_mult_6x2_2x1(float out[6], float in1[12], float in2[2])
 {
-	out[0] = in1[0]*in2[0] + in1[1]*in2[1];
-	out[1] = in1[2]*in2[0] + in1[3]*in2[1];
-	out[2] = in1[4]*in2[0] + in1[5]*in2[1];
-	out[3] = in1[6]*in2[0] + in1[7]*in2[1];
-	out[4] = in1[8]*in2[0] + in1[9]*in2[1];
-	out[5] = in1[10]*in2[0] + in1[11]*in2[1];
+  out[0] = in1[0]*in2[0] + in1[1]*in2[1];
+  out[1] = in1[2]*in2[0] + in1[3]*in2[1];
+  out[2] = in1[4]*in2[0] + in1[5]*in2[1];
+  out[3] = in1[6]*in2[0] + in1[7]*in2[1];
+  out[4] = in1[8]*in2[0] + in1[9]*in2[1];
+  out[5] = in1[10]*in2[0] + in1[11]*in2[1];
 }
 void mat_add_6x1(float out[6], float in1[6], float in2[6])
 {
-	out[0] = in1[0]+in2[0];
-	out[1] = in1[1]+in2[1];
-	out[2] = in1[2]+in2[2];
-	out[3] = in1[3]+in2[3];
-	out[4] = in1[4]+in2[4];
-	out[5] = in1[5]+in2[5];
+  out[0] = in1[0]+in2[0];
+  out[1] = in1[1]+in2[1];
+  out[2] = in1[2]+in2[2];
+  out[3] = in1[3]+in2[3];
+  out[4] = in1[4]+in2[4];
+  out[5] = in1[5]+in2[5];
 }
 void mat_mult_2x6_6x1(float out[2], float in1[12], float in2[6])
 {
-	out[0] = in1[0]*in2[0] +in1[1]*in2[1] + in1[2]*in2[2] + in1[3]*in2[3] + in1[4]*in2[4] + in1[5]*in2[5];
-	out[1] = in1[6]*in2[0] +in1[7]*in2[1] + in1[8]*in2[2] + in1[9]*in2[3] + in1[10]*in2[4] + in1[11]*in2[5];
+  out[0] = in1[0]*in2[0] +in1[1]*in2[1] + in1[2]*in2[2] + in1[3]*in2[3] + in1[4]*in2[4] + in1[5]*in2[5];
+  out[1] = in1[6]*in2[0] +in1[7]*in2[1] + in1[8]*in2[2] + in1[9]*in2[3] + in1[10]*in2[4] + in1[11]*in2[5];
 }
 void mat_subtract_2x1(float out[2], float in1[2], float in2[2])
 {
-	out[0] = in1[0]-in2[0];
-	out[1] = in1[1]-in2[1];
+  out[0] = in1[0]-in2[0];
+  out[1] = in1[1]-in2[1];
 }
 
 
@@ -414,8 +397,7 @@ static bool check_anchor_timeout(struct DW1000 *dw)
   return false;
 }
 
-
- void uwb_dw1000_init(void) {
+void local_and_comms_init(void) {
      // init DW1000 structure
     dw1000.idx = 0;
   //  dw1000.ck = 0;
@@ -452,43 +434,22 @@ static bool check_anchor_timeout(struct DW1000 *dw)
     // init trilateration algorithm !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //    trilateration_init(dw1000.anchors); //This is for trilateration
 //    multilateration_init(dw1000.anchors); //This is fo multilateration
- }//end of the init function
- 
- void uwb_dw1000_periodic(void) {
- 	// Check for timeout. If the beacons stop sending ranges, then this is called to set the GPS fix to false.
-  // This might casuse issues when using the datalink type GPS as it will switch between datalink and uwb.
-  	gps_periodic_check(&(dw1000.gps_dw1000));
- }//end of the periodic function
+}
 
- 
-void uwb_dw1000_resetheading(void) {
- // store current heading as ref and stop periodic call
-  dw1000.initial_heading = stateGetNedToBodyEulers_f()->psi;
-  uwb_dw1000_delft_uwb_dw1000_resetheading_status = MODULES_STOP;
-  
-}//end of the reset heading function
- 
- void uwb_dw1000_report(void) {
-	  // float buf[9];
-	  // buf[0] = dw1000.anchors[0].distance;
-	  // buf[1] = dw1000.anchors[1].distance;
-	  // buf[2] = dw1000.anchors[2].distance;
-	  // buf[3] = dw1000.raw_pos.x;
-	  // buf[4] = dw1000.raw_pos.y;
-	  // buf[5] = dw1000.raw_pos.z;
-	  // buf[6] = dw1000.pos.x;
-	  // buf[7] = dw1000.pos.y;
-	  // buf[8] = dw1000.pos.z;
-	  // DOWNLINK_SEND_PAYLOAD_FLOAT(DefaultChannel, DefaultDevice, 9, buf); 
+void local_and_comms_periodic(void) {
+	struct EnuCoor_f *pos2 = stateGetPositionEnu_f();
+  /*sendFloat(UWB_SERIAL_COMM_X, (*pos2).x);
+  sendFloat(UWB_SERIAL_COMM_Y, (*pos2).y);*/
+}
 
-      //This is used for system identification
-    struct EnuCoor_f *pos2 = stateGetPositionEnu_f();
+void local_and_comms_report(void) {
+	struct EnuCoor_f *pos2 = stateGetPositionEnu_f();
     struct EnuCoor_f *vel = stateGetSpeedEnu_f();
     printf("%f,%f,%f,%f,%f,%f,%f,%f,%f \n",dw1000.anchors[0].distance,dw1000.anchors[1].distance,dw1000.anchors[2].distance,dw1000.anchors[3].distance,(*pos2).x,(*pos2).y,(*pos2).z,(*vel).x,(*vel).y); //for identification
     
- }//end of the report function
- 
- void uwb_dw1000_event(void) {
+}
+
+void local_and_comms_event(void) {
     dw1000_arduino_parse(&dw1000);
   
  
@@ -510,10 +471,6 @@ void uwb_dw1000_resetheading(void) {
       {
         // printf("ERROR: trilateration failed \n");
       }
-      
-    
       dw1000.updated = false;
     }
- }//end of the event function
-
-
+}
---
+++ sw/airborne/modules/decawave/uwb_localisation_and_comms.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) Thomas Fijen
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/decawave/uwb_localisation_and_comms.h"
+ * @author Thomas Fijen
+ * This function contains the code necessary to localise the UAV, using UWB, as well as the inter-UAV communication
+ */
+
+#ifndef UWB_LOCALISATION_AND_COMMS_H
+#define UWB_LOCALISATION_AND_COMMS_H
+
+extern void local_and_comms_init(void);
+extern void local_and_comms_periodic(void);
+extern void local_and_comms_report(void);
+extern void local_and_comms_event(void);
+
+ extern void kalman_filter(float out[6], float X_old[6], float u[2], float x, float y);
+ extern void mat_mult_6x6_6x1(float out[6], float in1[36], float in2[6]);
+ extern void mat_mult_6x2_2x1(float out[6], float in1[12], float in2[2]);
+ extern void mat_add_6x1(float out[6], float in1[6], float in2[6]);
+ extern void mat_mult_2x6_6x1(float out[2], float in1[12], float in2[6]);
+ extern void mat_subtract_2x1(float out[2], float in1[2], float in2[2]);
+ 
+ extern void getRanges(float ranges[4]);
+
+#endif
+
