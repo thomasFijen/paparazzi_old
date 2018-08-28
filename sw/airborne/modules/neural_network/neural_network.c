110bd49fef76322d71a28711c1ba7f940fa78fa6
thomasFijen <thomasfijen@yahoo.com>
2018-08-28 14:42:06 +0200

    Code cleanup

---
+++ sw/airborne/modules/neural_network/neural_network.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) Thomas Fijen
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/neural_network/neural_network.c"
+ * @author Thomas Fijen
+ * This fuction implements the NN control necessary for my persistent surveillance mission. 
+ * The NN used is not fully connected and has two outputs, namely; the x and y velocities.
+ */
+
+#include "modules/neural_network/neural_network.h"
+
+void neural_network_init(void) {
+
+}
+
+void neural_network_periodic(void) {
+	
+}
+
+
